161ef896b24a11cd56aba3584267a2bb2db39a9b
Sanjaya Ilham <sanjayailham85@gmail.com>
2022-06-30 10:45:40 +0700

    add provider

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ import "remixicon/fonts/remixicon.css";
 import { createRoot } from 'react-dom/client';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import store from "./store/store";
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
