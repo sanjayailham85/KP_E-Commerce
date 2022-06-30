c719152d83a0c4cc80cbaefd8c007d33fc96bf79
Sanjaya Ilham <sanjayailham85@gmail.com>
2022-06-30 10:46:49 +0700

    change code

--- src/store/store.js
+++ src/store/store.js
@@ -1,45 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./shopping-cart/cartSlice";
 
-const initialState = {
-    cartItems: [],
-    totalQuantity: 0,
-    totalAmount: 0
-}
+const store = configureStore ({
+    reducer: {
+        cart: cartSlice.reducer,
+    },
+});
 
-const cartSlice = ({
-    name: 'cart',
-    initialState: initialState,
-
-    reducers: {
-        addItem(state, action) {
-            const newItem = action.payload
-            const existingItem = state.cartItems.find(item => item.id === newItem.id)
-            state.totalQuantity++
-
-            if(!existingItem){
-                
-                // Video part 01 - 3:08:00
-                state.cartItems.push({
-                    id: newItem.id,
-                    title: newItem.title,
-                    image01: newItem.image01,
-                    price: newItem.price,
-                    quantity: 1,
-                    totalPrice: newItem.price
-                })
-            }
-
-            else {
-                existingItem.quantity++
-                existingItem.totalPrice = Number(existingItem.totalPrice) + Number(newItem.price)
-            }
-
-            state.totalAmount = state.cartItems.reduce((total, item)=>(
-                total + Number(item.price) * Number(item.quantity)
-            ) )
-        }
-    }
-})
-
-export const cartAction = cartSlice.actions
-export default cartSlice
+export default store;
