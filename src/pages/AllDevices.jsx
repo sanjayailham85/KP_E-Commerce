4efb239010ef2f082b153c7c410ee31317b96b0b
Sanjaya Ilham <sanjayailham85@gmail.com>
2022-07-04 14:01:04 +0700

    Add Fitur

--- src/pages/AllDevices.jsx
+++ src/pages/AllDevices.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Helmet from '../components/Helmet/Helmet'
 import CommonSection from "../components/UI/common-section/CommonSection";
 import {Container, Row, Col} from 'reactstrap'
+import ReactPaginate from 'react-paginate'
 
 import products from '../assets/fake-data/products'
 import ProductCard from '../components/UI/product-card/ProductCard'
 
 import '../styles/all-devices.css'
+import '../styles/pagination.css'
 
 const AllDevices = () => {
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const [pageNumber, setPageNumber] = useState(0)
+
+    const productPerPage = 2
+    const visitedPage = pageNumber * productPerPage
+    const displayPage = products.slice(visitedPage, visitedPage + productPerPage)
+
+    const pageCount = Math.ceil(products.length / productPerPage)
+
+    const changePage = ({selected}) => {
+        setPageNumber(selected)
+    }
+    
+
     return <Helmet title="All-Devices">
         <CommonSection title="All Devices"/>
 
@@ -17,14 +34,14 @@ const AllDevices = () => {
                 <Row className="mt-4">
                     <Col lg="6" md="6" sm="6">
                         <div className="search__widget d-flex align-items-center justify-content-between w-50">
-                            <input type="text" placeholder="I'm looking for...."/>
+                            <input type="text" placeholder="I'm looking for...." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
                             <span><i className="ri-search-line"></i></span>
                         </div>
                     </Col>
 
                     <Col lg="6" md="6" sm="6" className="mb-5">
                     <div className="sorting__widget text-end">
-                        <select>
+                        <select className="w-50">
                             <option>Default</option>
                             <option value="ascending">Alphabetically, A-Z</option>
                             <option value="descending">Alphabetically, Z-A</option>
@@ -34,15 +51,30 @@ const AllDevices = () => {
                     </div>
                     </Col>
                     
-                    {
-                        products.map((item)=> (
+                    {displayPage
+                    
+                       ?.filter((item)=> {
+                        if(searchTerm.value === "") return item
+                        if(item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                            return item
+                        })
+                    
+                    .map((item)=> (
                             <Col lg="3" md="4" sm="6" key={item.id} className="mb-4">
                                 {" "} 
                                 <ProductCard item={item} />
-                            </Col>
-                            
-                        ))
-                    }
+                            </Col>  
+                        ))}
+
+                    <div >
+                        <ReactPaginate
+                            pageCount={pageCount}
+                            onPageChange={changePage}
+                            previousLabel='Prev'
+                            nextLabel='Next'
+                            containerClassName="paginationBttns"
+                            />
+                    </div>
 
                 </Row>
             </Container>
